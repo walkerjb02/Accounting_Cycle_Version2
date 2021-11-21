c0bc5f074443b59e4e600fb6d74c3c51bb779462
J. Brandon Walker <88942620+walkerjb02@users.noreply.github.com>
2021-11-21 15:19:40 -0500

    Create Storage.py

---
+++ Storage.py
@@ -0,0 +1,5 @@
+dbr = {'cash': 950, 'factory equipment': 1000, 'wage expense': 50, 'utilities expense': 100, 'ar': -500}
+cbr = {'stock': 2000, 'services revenue': 600}
+dje = ['11/23/2021', '11/24/2021', '11/26/2021']
+ret_earn = 0
+ni = 0
